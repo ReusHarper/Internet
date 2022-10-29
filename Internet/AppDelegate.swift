600dfc941e4b68493be6e8cfc85735e88ec73d52
ReusHarper <reusharper@comunidad.unam.mx>
2022-10-28 21:20:39 -0500

    Prueba de conectividad a Internet y petición a sitio web unam.mx

--- Internet/AppDelegate.swift
+++ Internet/AppDelegate.swift
@@ -6,15 +6,61 @@
 //
 
 import UIKit
+import Network
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    var internetStatus : Bool = false
+    var internetType: String = ""
 
-
-
+    // 1. La aplicacion se lanza por primera vez
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        let monitor = NWPathMonitor()
+        // Closure: Es un bloque de codigo que se ejecuta de manera asincrona
+        monitor.pathUpdateHandler = { path in
+            // path es un argumento que llega a la funcion
+            if path.status != .satisfied {
+                self.internetStatus = false
+            }
+            else {
+                self.internetStatus = true
+                
+                // TODO: Si hay internet, verificar el tipo
+                if path.usesInterfaceType(.wifi) {
+                    self.internetType = "WIFI"
+                }
+                else if path.usesInterfaceType(.cellular){
+                    self.internetType = "CELL"
+                }
+            }
+        }
+        
+        //
+        monitor.start(queue: DispatchQueue.global())
         return true
+    }
+    
+    // 2. El usuario cambia de aplicacion o llego una interrupcion del SO (Ej: Una llamada)
+    func applicationWillResignActive(_ application: UIApplication) {
+        print("")
+    }
+    
+    // 3. La app ya no esta activa pero sigue abierta
+    func applicationDidEnterBackground(_ application: UIApplication) {
+        print("")
+    }
+    
+    // 4. La app va a volver  a primer planoo
+    func applicationWillEnterForeground(_ application: UIApplication) {
+        print("")
+    }
+    
+    // 5. La app ya se reactiva (vuelve del 2do plano)
+    func applicationDidBecomeActive(_ application: UIApplication) {
+        print("")
     }
 
     // MARK: UISceneSession Lifecycle
--- Internet/ViewController.swift
+++ Internet/ViewController.swift
@@ -6,14 +6,47 @@
 //
 
 import UIKit
+import WebKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let ad = UIApplication.shared.delegate as! AppDelegate
+        
+        //
+        if ad.internetStatus {
+            // TODO:
+            // 1. Instanciar un objeto webview y colocarlo en la vista
+            let wv = WKWebView(frame: self.view.frame)
+            wv.allowsBackForwardNavigationGestures = true
+            self.view.addSubview(wv)
+            
+            // 2. Obtener una pagina web y cargarla en el objeto webview
+            if let URL = URL(string: "https://www.unam.mx") {
+                wv.load(URLRequest(url: URL))
+            }
+        }
+        else {
+            let ac = UIAlertController(
+                title: "Error",
+                message: "Lo sentimos, no cuenta con una conexion a Internet",
+                preferredStyle: .alert
+            )
+            let action = UIAlertAction(
+                title: "Enterado",
+                style: .default,
+                handler: nil
+            )
+            ac.addAction(action)
+            self.present(ac, animated: true)
+        }
     }
-
+    
+    override func viewDidAppear(_ animated: Bool) {
+        print("")
+    }
 
 }
 
